666743646233f7ba89fee81962836d8f26083cf6
Matija Pretnar <matija@pretnar.info>
2012-08-08 14:50:14 +0200

    Added an interface for Error.

--- src/error.ml
+++ src/error.ml
@@ -1,20 +1,14 @@
-(** Errors raised by eff. *)
-
-(** Each error has a position, a type (typing/runtime/...), and a message. *)
 exception Error of (Common.position * string * string)
 
-(** [error ~pos err_type msg] raises an error with an optional position [pos],
-    an error type [err_type], and a message [msg]. The [kfprintf] magic allows
-    one to write [msg] using a format string and its arguments. *)
+(** [error ~pos err_type msg] raises an error with a position [pos], an error
+    type [err_type], and a message [msg]. The [kfprintf] magic allows
+    one to write [msg] using a format string. *)
 let error ~pos err_type =
   let k _ =
     let msg = Format.flush_str_formatter () in
     raise (Error (pos, err_type, msg))
   in
   Format.kfprintf k Format.str_formatter
-
-(** Shortcut functions for raising the most common errors. For example, to
-    raise a typing error one writes [Error.typing "Unknown name %s." x]. *)
 
 let fatal ~pos = error ~pos "Fatal error"
 let syntax ~pos = error ~pos "Syntax error"
