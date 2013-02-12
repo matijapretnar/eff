b9aa2115d7eb69974871700d423d63e959e4397a
Matija Pretnar <matija@pretnar.info>
2013-02-12 21:45:47 +0100

    Added a Newprint module that is meant to replace Print.
    This module will contain only support for printing,
    but not all the pretty-printers. Each module will
    provide its own pretty-printers. This should clear
    up the dependencies.

---
+++ src/newprint.ml
@@ -0,0 +1,19 @@
+let fprintf = Format.fprintf
+
+let print ?(max_level=9999) ?(at_level=0) ppf =
+  if max_level < at_level then
+    begin
+      Format.fprintf ppf "(@[";
+      Format.kfprintf (fun ppf -> Format.fprintf ppf "@])") ppf
+    end
+  else
+    begin
+      Format.fprintf ppf "@[";
+      Format.kfprintf (fun ppf -> Format.fprintf ppf "@]") ppf
+    end
+
+let rec sequence sep pp vs ppf =
+  match vs with
+  | [] -> ()
+  | [v] -> pp v ppf
+  | v :: vs -> Format.fprintf ppf "%t%s@ %t" (pp v) sep (sequence sep pp vs)
