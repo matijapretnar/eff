ac50dc8d53343e4dbddea59342dc10cd31632edb
Matija Pretnar <matija@pretnar.info>
2015-02-20 17:03:45 +0100

    Compiler that goes through intermeddiate AST

--- src/compiler/dynamicFree.ml
+++ src/compiler/dynamicFree.ml
@@ -73,7 +73,7 @@ and compile_computation (c, pos) =
   | Syntax.Check c ->
       Check (compile_computation c)
   in (c', pos)
-and compile_variable (n, x) = (x ^ string_of_int n)
+and compile_variable (n, x) = x
 and compile_pattern p = Pattern.map compile_variable p
 and compile_abstraction (p, c) = (compile_pattern p, compile_computation c)
 and compile_abstraction2 (p1, p2, c) =
@@ -81,9 +81,9 @@ and compile_abstraction2 (p1, p2, c) =
 and compile_operation (e, op) = (compile_expression e, op)
 and compile_handler h = {
   operations =
-    List.map (fun (op, a2) -> (compile_operation op, compile_abstraction2 a2)) h.operations;
-  value = compile_abstraction h.value;
-  finally = compile_abstraction h.finally;
+    List.map (fun (op, a2) -> (compile_operation op, compile_abstraction2 a2)) h.Syntax.operations;
+  value = compile_abstraction h.Syntax.value;
+  finally = compile_abstraction h.Syntax.finally;
 }
 and compile_let lst =
   List.map (fun (p, c) -> (compile_pattern p, compile_computation c)) lst
@@ -94,72 +94,66 @@ and compile_letrec lst =
 let rec print_pattern ?max_level (p,_) ppf =
   let print ?at_level = Print.print ?max_level ?at_level ppf in
   match p with
-  | Pattern.Var x -> print "(%t)" (Print.variable x)
-  | Pattern.As (p, x) -> print ~at_level:2 "%t as %t" (print_pattern p) (Print.variable x)
+  | Pattern.Var x -> print "(%s)" x
+  | Pattern.As (p, x) -> print ~at_level:2 "%t as %s" (print_pattern p) x
   | Pattern.Const c -> Common.print_const c ppf
   | Pattern.Tuple lst -> Print.tuple print_pattern lst ppf
   | Pattern.Record lst -> Print.record print_pattern lst ppf
-  | Pattern.Variant (lbl, None) when lbl = Common.nil -> print "[]"
+  (* | Pattern.Variant (lbl, None) when lbl = Common.nil -> print "[]" *)
   | Pattern.Variant (lbl, None) -> print "%s" lbl
-  | Pattern.Variant (lbl, Some (Pattern.Tuple [v1; v2], _)) when lbl = Common.cons ->
+(*   | Pattern.Variant (lbl, Some (Pattern.Tuple [v1; v2], _)) when lbl = Common.cons ->
       print "(%t) :: (%t)" (print_pattern v1) (print_pattern v2)
+ *)
   | Pattern.Variant (lbl, Some p) ->
       print ~at_level:1 "%s @[<hov>%t@]" lbl (print_pattern p)
   | Pattern.Nonbinding -> print "_"
 
-let rec print_computation ?max_level c ppf =
+let rec print_term ?max_level c ppf =
   let print ?at_level = Print.print ?max_level ?at_level ppf in
   match fst c with
-  | Syntax.Apply (e1, e2) -> print ~at_level:1 "%t %t" (print_expression e1) (print_expression ~max_level:0 e2)
-  | Syntax.Value e -> print ~at_level:1 "value %t" (print_expression ~max_level:0 e)
-  | Syntax.Match (e, lst) -> print "match %t with @[<hov>%t@]" (print_expression e) (Print.sequence "\n" case lst)
-  | Syntax.While (c1, c2) -> Error.runtime "Compiling of while loops not implemented"
-  | Syntax.For (i, e1, e2, c, d) -> Error.runtime "Compiling of for loops not implemented"
-  | Syntax.New (eff, _) -> print "value (new_instance ())"
-  (* XXX Do compilation of resources *)
-  | Syntax.Handle (e, c) -> print ~at_level:1 "handle (%t) (%t)" (print_expression e) (print_computation ~max_level:0 c)
-  | Syntax.Let (lst, c) -> print "%t" (compile_let c lst)
-  | Syntax.LetRec (lst, c) -> print "let rec @[<hov>%t@] in %t" (Print.sequence " and " let_rec_abstraction lst) (print_computation c)
-  | Syntax.Check c -> print "()"
+  | Apply (e1, e2) -> print ~at_level:1 "%t %t" (print_term e1) (print_term ~max_level:0 e2)
+  | Value e -> print ~at_level:1 "value %t" (print_term ~max_level:0 e)
+  | Match (e, lst) -> print "match %t with @[<hov>%t@]" (print_term e) (Print.sequence "\n" case lst)
+  | New -> print "value (new_instance ())"
+  | Handle (e, c) -> print ~at_level:1 "handle (%t) (%t)" (print_term e) (print_term ~max_level:0 c)
+  | Let (lst, c) -> print "%t" (print_let c lst)
+  | LetRec (lst, c) -> print "let rec @[<hov>%t@] in %t" (Print.sequence " and " let_rec_abstraction lst) (print_term c)
+  | Check c -> print "()"
+  | Var x -> print "(%s)" x
+  | Const c -> print "%t" (Common.print_const c)
+  | Tuple lst -> Print.tuple print_term lst ppf
+  | Record lst -> Print.record print_term lst ppf
+  | Variant (lbl, None) -> print "%s" lbl
+  | Variant (lbl, Some e) ->
+    print ~at_level:1 "%s @[<hov>%t@]" lbl (print_term e)
+  | Lambda a -> print ~at_level:3 "fun %t" (abstraction a)
+  | Handler h -> print "%t" (handler h)
+  | Operation op -> print "apply_operation %t" (operation op)
 
-and compile_let c2 lst ppf =
+and print_let c2 lst ppf =
   let print ?at_level = Print.print ?at_level ppf in
   match lst with
-  | [] -> print "%t" (print_computation c2)
-  | (p, c1) :: lst -> print "((%t) >> (fun %t -> %t))" (print_computation c1) (print_pattern p) (compile_let c2 lst)
-
-and print_expression ?max_level e ppf =
-  let print ?at_level = Print.print ?max_level ?at_level ppf in
-  match fst e with
-  | Syntax.Var x -> print "(%t)" (Print.variable x)
-  | Syntax.Const c -> print "%t" (Common.print_const c)
-  | Syntax.Tuple lst -> Print.tuple print_expression lst ppf
-  | Syntax.Record lst -> Print.record print_expression lst ppf
-  | Syntax.Variant (lbl, None) -> print "%s" lbl
-  | Syntax.Variant (lbl, Some e) ->
-    print ~at_level:1 "%s @[<hov>%t@]" lbl (print_expression e)
-  | Syntax.Lambda a -> print ~at_level:3 "fun %t" (abstraction a)
-  | Syntax.Handler h  -> print "%t" (handler h)
-  | Syntax.Operation op -> print "apply_operation %t" (operation op)
+  | [] -> print "%t" (print_term c2)
+  | (p, c1) :: lst -> print "((%t) >> (fun %t -> %t))" (print_term c1) (print_pattern p) (print_let c2 lst)
 
 and operation (e, op) ppf =
-  Print.print ppf "(\"%s\", %t)" op (print_expression e)
+  Print.print ppf "(\"%s\", %t)" op (print_term e)
 
 and abstraction (p, c) ppf =
-  Format.fprintf ppf "%t -> %t" (print_pattern p) (print_computation c)
+  Format.fprintf ppf "%t -> %t" (print_pattern p) (print_term c)
 
 and let_abstraction (p, c) ppf =
-  Format.fprintf ppf "%t = %t" (print_pattern p) (print_computation c)
+  Format.fprintf ppf "%t = %t" (print_pattern p) (print_term c)
 
 and let_rec_abstraction (x, a) ppf =
-  Format.fprintf ppf "%t = fun %t" (Print.variable x) (abstraction a)
+  Format.fprintf ppf "%s = fun %t" x (abstraction a)
 
 and case a ppf =
   Format.fprintf ppf "| %t" (abstraction a)
 
 and handler h ppf =
   Format.fprintf ppf "{ value = (fun %t); finally = (fun %t); operation_cases = (%t)}"
-  (abstraction h.Syntax.value) (abstraction h.Syntax.finally) (operation_cases h.Syntax.operations)
+  (abstraction h.value) (abstraction h.finally) (operation_cases h.operations)
 
 and operation_cases cases ppf =
   match cases with
@@ -167,10 +161,11 @@ and operation_cases cases ppf =
   | (op, (p, k, c)) :: cases ->
       Format.fprintf ppf
       "Cons (%t, (fun %t %t -> %t), %t)"
-      (operation op) (print_pattern p) (print_pattern k) (print_computation c) (operation_cases cases)
+      (operation op) (print_pattern p) (print_pattern k) (print_term c) (operation_cases cases)
 
 let compile c =
-  print_computation c Format.str_formatter;
+  let c' = compile_computation c in
+  print_term c' Format.str_formatter;
   Format.flush_str_formatter ()
 
 let compile_with_header c =
