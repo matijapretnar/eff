b2bce49b4df81d5fd044b8e12991f6fed2516e47
Matija Pretnar <matija@pretnar.info>
2015-06-05 12:32:21 +0100

    Created a single record for the toplevel state

--- src/eff.ml
+++ src/eff.ml
@@ -72,10 +72,20 @@ let parse parser lex =
   | Failure "lexing: empty token" ->
       Error.syntax ~loc:(Location.of_lexeme lex) "unrecognised symbol."
 
-let initial_ctxenv =
-  ((Ctx.empty, Common.id), Eval.initial)
 
-let infer_top_comp (ctx, top_change) c =
+type state = {
+  environment : Eval.env;
+  context : Ctx.t;
+  change : Scheme.dirty_scheme -> Scheme.dirty_scheme;
+}
+
+let initial_ctxenv = {
+  environment = Eval.initial;
+  context = Ctx.empty;
+  change = Common.id;
+}
+
+let infer_top_comp ctx top_change c =
   let ctx', (ty', drt'), cnstrs' = Infer.infer_comp ctx c in
   let change = Scheme.add_to_top ~loc:(snd c) ctx' cnstrs' in
   let top_change = Common.compose top_change change in
@@ -91,38 +101,39 @@ let infer_top_comp (ctx, top_change) c =
 (* [exec_cmd env c] executes toplevel command [c] in global
     environment [(ctx, env)]. It prints the result on standard output
     and return the new environment. *)
-let rec exec_cmd interactive ((ctx, top_change) as wholectx, env) (d,loc) =
+let rec exec_cmd interactive st (d,loc) =
   match d with
   | SugaredSyntax.Term c ->
       let c = Desugar.top_computation c in
-      let drty_sch, top_change = infer_top_comp wholectx c in
-      let v = Eval.run env c in
+      let drty_sch, new_change = infer_top_comp st.context st.change c in
+      let v = Eval.run st.environment c in
       if interactive then Format.printf "@[- : %t = %t@]@."
         (Scheme.print_dirty_scheme drty_sch)
         (Value.print_value v);
-      ((ctx, top_change), env)
+      {st with change = new_change}
   | SugaredSyntax.TypeOf c ->
       let c = Desugar.top_computation c in
-      let drty_sch, top_change = infer_top_comp wholectx c in
+      let drty_sch, new_change = infer_top_comp st.context st.change c in
       Format.printf "@[- : %t@]@." (Scheme.print_dirty_scheme drty_sch);
-      ((ctx, top_change), env)
+      {st with change = new_change}
   | SugaredSyntax.Reset ->
       Tctx.reset ();
       print_endline ("Environment reset."); initial_ctxenv
   | SugaredSyntax.Help ->
-      print_endline help_text; (wholectx, env)
+      print_endline help_text;
+      st
   | SugaredSyntax.Quit -> exit 0
-  | SugaredSyntax.Use fn -> use_file (wholectx, env) (fn, interactive)
+  | SugaredSyntax.Use fn -> use_file st (fn, interactive)
   | SugaredSyntax.TopLet defs ->
       let defs = Desugar.top_let defs in
       (* XXX What to do about the dirts? *)
-      let vars, nonpoly, change = Infer.infer_let ~loc ctx defs in
-      let ctx = List.fold_right (fun (x, ty_sch) env -> Ctx.extend env x ty_sch) vars ctx in
+      let vars, nonpoly, change = Infer.infer_let ~loc st.context defs in
+      let ctx = List.fold_right (fun (x, ty_sch) env -> Ctx.extend env x ty_sch) vars st.context in
       let extend_nonpoly (x, ty) env =
         (x, ([(x, ty)], ty, Constraints.empty)) :: env
       in
       let vars = List.fold_right extend_nonpoly nonpoly vars in
-      let top_change = Common.compose top_change change in
+      let top_change = Common.compose st.change change in
       let sch_change (ctx, ty, cnstrs) =
         let (ctx, (ty, _), cnstrs) = top_change (ctx, (ty, Type.fresh_dirt ()), cnstrs) in
         (ctx, ty, cnstrs)
@@ -131,7 +142,7 @@ let rec exec_cmd interactive ((ctx, top_change) as wholectx, env) (d,loc) =
       let env =
         List.fold_right
           (fun (p,c) env -> let v = Eval.run env c in Eval.extend p v env)
-          defs env
+          defs st.environment
       in
         if interactive then begin
           List.iter (fun (x, tysch) ->
@@ -141,33 +152,45 @@ let rec exec_cmd interactive ((ctx, top_change) as wholectx, env) (d,loc) =
                          Format.printf "@[val %t : %t = %t@]@." (Syntax.print_variable x) (Scheme.print_ty_scheme (sch_change tysch)) (Value.print_value v))
             vars
         end;
-        ((ctx, top_change), env)
+        {
+          context = ctx;
+          change = top_change;
+          environment = env;
+        }
     | SugaredSyntax.TopLetRec defs ->
         let defs = Desugar.top_let_rec defs in
-        let vars, change = Infer.infer_let_rec ~loc ctx defs in
-        let ctx = List.fold_right (fun (x, ty_sch) env -> Ctx.extend ctx x ty_sch) vars ctx in
-        let top_change = Common.compose top_change change in
+        let vars, change = Infer.infer_let_rec ~loc st.context defs in
+        let ctx = List.fold_right (fun (x, ty_sch) ctx -> Ctx.extend ctx x ty_sch) vars st.context in
+        let top_change = Common.compose st.change change in
         let sch_change (ctx, ty, cnstrs) =
           let (ctx, (ty, _), cnstrs) = top_change (ctx, (ty, Type.fresh_dirt ()), cnstrs) in
           (ctx, ty, cnstrs)
         in
         List.iter (fun (_, (p, c)) -> Exhaust.is_irrefutable p; Exhaust.check_comp c) defs ;
-        let env = Eval.extend_let_rec env defs in
+        let env = Eval.extend_let_rec st.environment defs in
           if interactive then begin
             List.iter (fun (x, tysch) -> Format.printf "@[val %t : %t = <fun>@]@." (Syntax.print_variable x) (Scheme.print_ty_scheme (sch_change tysch))) vars
           end;
-          ((ctx, top_change), env)
+        {
+          context = ctx;
+          change = top_change;
+          environment = env;
+        }
     | SugaredSyntax.External (x, t, f) ->
       let (x, t) = Desugar.external_ty (Tctx.is_effect ~loc) x t in
-      let ctx = Ctx.extend ctx x t in
+      let ctx = Ctx.extend st.context x t in
         begin match C.lookup f External.values with
-          | Some v -> ((ctx, top_change), Eval.update x v env)
+          | Some v -> {
+              context = ctx;
+              change = st.change;
+              environment = Eval.update x v st.environment;
+            }
           | None -> Error.runtime "unknown external symbol %s." f
         end
     | SugaredSyntax.Tydef tydefs ->
         let tydefs = Desugar.tydefs ~loc tydefs in
         Tctx.extend_tydefs ~loc tydefs ;
-        ((ctx, top_change), env)
+        st
 
 
 and use_file env (filename, interactive) =
