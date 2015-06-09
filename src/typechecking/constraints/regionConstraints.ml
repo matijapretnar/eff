c402491bd610ce4f6a1e48c2b4c5220050251a0d
Matija Pretnar <matija@pretnar.info>
2015-06-09 10:03:17 +0200

    Moving region constraints to the simpler poset

--- src/typechecking/constraints/regionConstraints.ml
+++ src/typechecking/constraints/regionConstraints.ml
@@ -1,6 +1,7 @@
-module RegionPoset = SkelPoset.Make(struct
+module RegionPoset = Poset.Make(struct
   type t = Type.region_param
   let compare = Pervasives.compare
+  let print = Type.print_region_param ~non_poly:Trio.empty
 end)
 
 module FullRegions = Set.Make(struct
@@ -20,7 +21,7 @@ let empty = {
 
 let union bnds1 bnds2 = {
   full_regions = FullRegions.union bnds1.full_regions bnds2.full_regions;
-  regions = RegionPoset.union bnds1.regions bnds2.regions;
+  regions = RegionPoset.merge bnds1.regions bnds2.regions;
 }
 
 let subst sbst bnds = {
@@ -31,21 +32,21 @@ let subst sbst bnds = {
 let garbage_collect pos neg bnds =
   {
     full_regions = FullRegions.filter (fun r -> List.mem r pos) bnds.full_regions;
-    regions = RegionPoset.garbage_collect pos neg bnds.regions;
+    regions = RegionPoset.filter (fun x y -> List.mem x neg && List.mem y pos && not (FullRegions.mem y bnds.full_regions)) bnds.regions;
   }
 
 let add_full_region r bnds =
-  (* REMOVE FULL REGIONS FROM bnds.regions *)
-  {bnds with full_regions = FullRegions.add r bnds.full_regions}
+  let new_full_regions = r :: RegionPoset.get_succ r bnds.regions in
+  {bnds with full_regions = List.fold_right FullRegions.add new_full_regions bnds.full_regions}
 
 let add_region_constraint r1 r2 bnds =
   if FullRegions.mem r1 bnds.full_regions then
     let new_full_regions = r2 :: RegionPoset.get_succ r2 bnds.regions in
-    List.fold_right add_full_region new_full_regions bnds
+    {bnds with full_regions = List.fold_right FullRegions.add new_full_regions bnds.full_regions}
   else
-    {bnds with regions = RegionPoset.add_edge r1 r2 bnds.regions}
+    {bnds with regions = RegionPoset.add r1 r2 bnds.regions}
 
 let print ~non_poly bnds ppf =
   Print.print ppf "%t; FULL: %t"
-    (RegionPoset.print (Type.print_region_param ~non_poly) bnds.regions)
+    (RegionPoset.print bnds.regions)
     (Print.sequence "," (Type.print_region_param ~non_poly) (FullRegions.elements bnds.full_regions))
