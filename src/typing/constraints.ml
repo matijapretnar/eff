4b64a09aef8f8c021136cd2d2e928154ec9b8f9c
Matija Pretnar <matija@pretnar.info>
2015-06-16 12:03:58 +0200

    Clean up Constraints

--- src/typing/constraints.ml
+++ src/typing/constraints.ml
@@ -22,72 +22,71 @@ module FullRegions = Set.Make(struct
 end)
 
 type t = {
-  tys : TyPoset.t;
-  dirts: DirtPoset.t;
-  regions: RegionPoset.t;
+  ty_poset : TyPoset.t;
+  dirt_poset: DirtPoset.t;
+  region_poset: RegionPoset.t;
   full_regions: FullRegions.t;
 }
 
 let empty = {
-  tys = TyPoset.empty;
-  dirts = DirtPoset.empty;
-  regions = RegionPoset.empty;
+  ty_poset = TyPoset.empty;
+  dirt_poset = DirtPoset.empty;
+  region_poset = RegionPoset.empty;
   full_regions = FullRegions.empty;
 }
 
-let union bnds1 bnds2 = {
-  tys = TyPoset.merge bnds1.tys bnds2.tys;
-  dirts = DirtPoset.merge bnds1.dirts bnds2.dirts;
-  regions = RegionPoset.merge bnds1.regions bnds2.regions;
-  full_regions = FullRegions.union bnds1.full_regions bnds2.full_regions;
+let union constraints1 constraints2 = {
+  ty_poset = TyPoset.merge constraints1.ty_poset constraints2.ty_poset;
+  dirt_poset = DirtPoset.merge constraints1.dirt_poset constraints2.dirt_poset;
+  region_poset = RegionPoset.merge constraints1.region_poset constraints2.region_poset;
+  full_regions = FullRegions.union constraints1.full_regions constraints2.full_regions;
 }
 
-let subst sbst bnds = {
-  tys = TyPoset.map sbst.Type.ty_param bnds.tys;
-  dirts = DirtPoset.map sbst.Type.dirt_param bnds.dirts;
-  regions = RegionPoset.map sbst.Type.region_param bnds.regions;
-  full_regions = FullRegions.fold (fun r s -> FullRegions.add (sbst.Type.region_param r) s) bnds.full_regions FullRegions.empty;
+let subst sbst constraints = {
+  ty_poset = TyPoset.map sbst.Type.ty_param constraints.ty_poset;
+  dirt_poset = DirtPoset.map sbst.Type.dirt_param constraints.dirt_poset;
+  region_poset = RegionPoset.map sbst.Type.region_param constraints.region_poset;
+  full_regions = FullRegions.fold (fun r s -> FullRegions.add (sbst.Type.region_param r) s) constraints.full_regions FullRegions.empty;
 }
 
-let garbage_collect (pos_ts, pos_ds, pos_rs) (neg_ts, neg_ds, neg_rs) bnds = {
-  tys = TyPoset.filter (fun x y -> List.mem x neg_ts && List.mem y pos_ts) bnds.tys;
-  dirts = DirtPoset.filter (fun x y -> List.mem x neg_ds && List.mem y pos_ds) bnds.dirts;
-  regions = RegionPoset.filter (fun x y -> List.mem x neg_rs && List.mem y pos_rs && not (FullRegions.mem y bnds.full_regions)) bnds.regions;
-  full_regions = FullRegions.filter (fun r -> List.mem r pos_rs) bnds.full_regions;
+let garbage_collect (pos_ts, pos_ds, pos_rs) (neg_ts, neg_ds, neg_rs) constraints = {
+  ty_poset = TyPoset.filter (fun x y -> List.mem x neg_ts && List.mem y pos_ts) constraints.ty_poset;
+  dirt_poset = DirtPoset.filter (fun x y -> List.mem x neg_ds && List.mem y pos_ds) constraints.dirt_poset;
+  region_poset = RegionPoset.filter (fun x y -> List.mem x neg_rs && List.mem y pos_rs && not (FullRegions.mem y constraints.full_regions)) constraints.region_poset;
+  full_regions = FullRegions.filter (fun r -> List.mem r pos_rs) constraints.full_regions;
 }
 
-let add_ty_constraint t1 t2 bnds =
-  {bnds with tys = TyPoset.add t1 t2 bnds.tys}
+let add_ty_constraint t1 t2 constraints =
+  {constraints with ty_poset = TyPoset.add t1 t2 constraints.ty_poset}
 
-let remove_ty t bnds =
-  let smaller, greater, tys = TyPoset.remove t bnds.tys in
-  smaller, greater, {bnds with tys = tys}
+let remove_ty t constraints =
+  let smaller, greater, ty_poset = TyPoset.remove t constraints.ty_poset in
+  smaller, greater, {constraints with ty_poset = ty_poset}
 
-let skeletons bnds =
+let skeletons constraints =
   (* XXX Not yet implemented *)
   []
 
-let add_dirt_constraint d1 d2 bnds =
-  {bnds with dirts = DirtPoset.add d1 d2 bnds.dirts}
+let add_dirt_constraint d1 d2 constraints =
+  {constraints with dirt_poset = DirtPoset.add d1 d2 constraints.dirt_poset}
 
-let remove_dirt d bnds =
-  let smaller, greater, dirts = DirtPoset.remove d bnds.dirts in
-  smaller, greater, {bnds with dirts = dirts}
+let remove_dirt d constraints =
+  let smaller, greater, dirt_poset = DirtPoset.remove d constraints.dirt_poset in
+  smaller, greater, {constraints with dirt_poset = dirt_poset}
 
-let add_region_constraint r1 r2 bnds =
-  if FullRegions.mem r1 bnds.full_regions then
-    let new_full_regions = r2 :: RegionPoset.get_succ r2 bnds.regions in
-    {bnds with full_regions = List.fold_right FullRegions.add new_full_regions bnds.full_regions}
+let add_full_region r constraints =
+  let new_full_regions = r :: RegionPoset.get_succ r constraints.region_poset in
+  {constraints with full_regions = List.fold_right FullRegions.add new_full_regions constraints.full_regions}
+
+let add_region_constraint r1 r2 constraints =
+  if FullRegions.mem r1 constraints.full_regions then
+    add_full_region r2 constraints
   else
-    {bnds with regions = RegionPoset.add r1 r2 bnds.regions}
+    {constraints with region_poset = RegionPoset.add r1 r2 constraints.region_poset}
 
-let add_full_region r bnds =
-  let new_full_regions = r :: RegionPoset.get_succ r bnds.regions in
-  {bnds with full_regions = List.fold_right FullRegions.add new_full_regions bnds.full_regions}
-
-let print ~non_poly bnds ppf =
-  Print.print ppf "TYS: %t\nDIRTS: %t\nREGIONS: %t\n; FULL: %t"
-    (TyPoset.print bnds.tys)
-    (DirtPoset.print bnds.dirts)
-    (RegionPoset.print bnds.regions)
-    (Print.sequence "," (Type.print_region_param ~non_poly) (FullRegions.elements bnds.full_regions))
+let print ~non_poly constraints ppf =
+  Print.print ppf "ty_poset: %t\ndirt_poset: %t\nregion_poset: %t\n; FULL: %t"
+    (TyPoset.print constraints.ty_poset)
+    (DirtPoset.print constraints.dirt_poset)
+    (RegionPoset.print constraints.region_poset)
+    (Print.sequence "," (Type.print_region_param ~non_poly) (FullRegions.elements constraints.full_regions))
