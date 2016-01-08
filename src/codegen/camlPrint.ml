0e770b63ebc0457e74adc175be688484ee8d4473
Matija Pretnar <matija@pretnar.info>
2016-01-08 10:02:01 +0100

    Fix printing of simultaneous binds

--- src/codegen/camlPrint.ml
+++ src/codegen/camlPrint.ml
@@ -55,8 +55,7 @@ and print_computation ?max_level c ppf =
   | Typed.Handle (e, c) ->
       print ~at_level:1 "handle %t %t" (print_expression ~max_level:0 e) (print_computation ~max_level:0 c)
   | Typed.Let (lst, c) ->
-      print ~at_level:2 "let @[<hov>%t@] in %t"
-      (Print.sequence " and " print_let_abstraction lst) (print_computation c)
+      print ~at_level:2 "%t" (print_multiple_bind (lst, c))
   | Typed.LetRec (lst, c) ->
       print ~at_level:2 "let rec @[<hov>%t@] in %t"
       (Print.sequence " and " print_let_rec_abstraction lst) (print_computation c)
@@ -84,6 +83,13 @@ and print_abstraction {Typed.term = (p, c)} ppf =
 
 and print_pure_abstraction {Typed.term = (p, e)} ppf =
   Format.fprintf ppf "%t -> (* pure *) %t" (print_pattern p) (print_expression e)
+
+and print_multiple_bind (lst, c') ppf =
+  match lst with
+  | [] -> Format.fprintf ppf "%t" (print_computation c')
+  | (p, c) :: lst ->
+      Format.fprintf ppf "%t >> fun %t -> %t"
+      (print_computation c) (print_pattern p) (print_multiple_bind (lst, c'))
 
 and print_let_abstraction (p, c) ppf =
   Format.fprintf ppf "%t = %t" (print_pattern p) (print_computation c)
