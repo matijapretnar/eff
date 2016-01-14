e78b66a9ecc8680d091dea54b6131e014bfced3a
amorotto <amorotto@gmail.com>
2016-01-14 12:47:00 +0100

    updated optimizer

--- src/codegen/optimize.ml
+++ src/codegen/optimize.ml
@@ -1,62 +1,107 @@
 open Typed
 
+
+
 let rec optimize_comp c =
   match c.term with
+
+  | Let (pclist,c2) -> let [(p1,c1)] = pclist in
+                        optimize_comp (bind ~loc:c.location c1 (abstraction ~loc:c.location p1 c2))
+
+(*  | Let (pclist,c2) -> folder pclist c2 *)
   | Bind (c1, c2) ->
-    begin match (optimize_comp c1).term with
+    begin match c1.term with
     (*Bind x (Value e) c -> LetC x e c*)
-    | Value e -> let_in ~loc:c.location e (optimize_abstraction c2)
+    | Value e -> optimize_comp (let_in ~loc:c.location e c2)
     | Bind (c3,c4) -> let (p1,cp1) = c2.term in 
                       let (p2,cp2) = c4.term in 
-                      bind ~loc:c.location c1 (abstraction ~loc:c.location p2 
-                                              (bind ~loc:c.location cp2 (abstraction ~loc:c.location p1 cp1)))
-
-    | _ -> c
+                      optimize_inner_comp (bind ~loc:c.location c1 (abstraction ~loc:c.location p2 
+                                              (bind ~loc:c.location cp2 (abstraction ~loc:c.location p1 cp1))))
+    | LetIn(e,a) -> let (p1,cp1) = c2.term in
+                   let (pa,ca) = a.term in
+                   let newbind = bind ~loc:c.location ca c2 in 
+                   let let_abs = abstraction ~loc:c.location pa newbind in
+                   optimize_inner_comp (let_in ~loc:c.location e let_abs)
+    
+    | _ -> optimize_inner_comp c
     end
   | Handle (e1,c1) ->
-    begin match (optimize_comp c1).term with
+    begin match c1.term with
 
     (*Handle h (LetC x e c) -> LetC (x e) (Handle c h)*)
     | LetIn (e2,a) -> let (p,c2) = a.term in 
+                    optimize_comp(
                       let_in ~loc:c.location e2 (abstraction ~loc:c.location p (handle ~loc:c.location e1 c2))
+                    )
     
     | Value v -> begin match e1.term with
     
                  (*Handle (Handler vc ocs) (Value v) -> Apply (Lambda vc) v *)
-                 | Handler h -> apply ~loc:c.location (lambda ~loc:c.location h.value_clause) v
+                 | Handler h -> optimize_comp(
+                                apply ~loc:c.location (lambda ~loc:c.location h.value_clause) v)
                  | _-> c
                  end
-    | _ -> c
+    | _ -> optimize_inner_comp c
     end
   | Apply (e1,e2) ->
-     begin match (optimize_expr e1).term with
+     begin match e1.term with
      (*Apply (PureLambda a) e -> Value (PureApply (PureLambda a) e)*)
      | PureLambda pure_abs -> let (p,e) = pure_abs.term in 
-                                 value ~loc:c.location 
-                                 (pure_apply ~loc:c.location 
-                                             (pure_lambda ~loc:c.location pure_abs)
-                                             (optimize_expr e2) ) 
-     | _ -> apply ~loc:c.location (optimize_expr e1) (optimize_expr e2)
+                                 optimize_comp 
+                                 (value ~loc:c.location 
+                                 (pure_apply ~loc:c.location (pure_lambda ~loc:c.location pure_abs) e2 )) 
+     | _ -> optimize_inner_comp c
      end
   
   | LetIn (e,a) ->
       let (p,cp) = a.term in
-      begin match (optimize_comp cp).term with
-      | Value e2 -> value ~loc:c.location (pure_let_in ~loc:c.location e (pure_abstraction ~loc:c.location p e2))
-      | _ -> let_in ~loc:c.location (optimize_expr e) (optimize_abstraction a)
+      begin match cp.term with
+      | Value e2 -> optimize_comp (value ~loc:c.location (pure_let_in ~loc:c.location e (pure_abstraction ~loc:c.location p e2)))
+      | _ -> optimize_inner_comp c
       end
 
-  | _ -> c
+  | _ -> optimize_inner_comp c
 
 and optimize_abstraction abs = let (p,c) = abs.term in abstraction ~loc:abs.location p (optimize_comp c) 
 
+and optimize_pure_abstraction abs = let (p,e) = abs.term in pure_abstraction ~loc:abs.location p (optimize_expr e)
+(*and folder pclist cn = 
+    let func = \a -> \b ->  (bind ~loc:b.location (abstraction ~loc:b.location (fst a) (snd a)) b)
+    in  list.fold_right(func , pclist, cn)
+*)
 and  optimize_expr e =
   match e.term with
   | Lambda a -> 
     let (p,c) = a.term in
     begin match c.term with 
     (*Lambda (x, Value e) -> PureLambda (x, e)*)
-    | Value v -> pure_lambda ~loc:e.location (pure_abstraction ~loc:e.location p v)
-    | _ -> e
+    | Value v -> optimize_expr (pure_lambda ~loc:e.location (pure_abstraction ~loc:e.location p v))
+    | _ -> optimize_inner_expr e
     end
+  | _ -> optimize_inner_expr e
+
+and optimize_inner_comp c =
+  match c.term with
+  | Value e -> value ~loc:c.location (optimize_expr e)
+(*  | LetRec li c1 -> let_rec' ~loc:c.location li (optimize_comp c1)
+  | Match e li -> match' ~loc:c.location (optimize_expr e) li
+  | While c1 c2 -> while' ~loc:c.location (optimize_comp c1) (optimize_comp c2)
+  | For v e1 e2 c1 b -> for' ~loc:c.location v (optimize_expr e1) (optimize_expr e2) (optimize_comp c1) b
+*)  | Apply (e1, e2) -> apply ~loc:c.location (optimize_expr e1) (optimize_expr e2)
+  | Handle (e, c1) -> handle ~loc:c.location (optimize_expr e) (optimize_comp c1)
+  | Check c1 -> check ~loc:c.location (optimize_comp c1)
+ (* | Call (eff, e1, a1) -> call ~loc:c.location eff (optimize_expr e1) (optimize_abstraction a1) *)
+  | Bind (c1, a1) -> bind ~loc:c.location (optimize_comp c1) (optimize_abstraction a1)
+  | LetIn (e, a) -> let_in ~loc: c.location(optimize_expr e) (optimize_abstraction a)
+  | _ -> c
+
+and optimize_inner_expr e =
+  match e.term with
+  | Var v -> e
+  | Const c -> const ~loc:e.location c
+  | Tuple lst -> tuple ~loc:e.location lst
+  | Lambda a -> lambda ~loc:e.location (optimize_abstraction a)
+  | PureLambda pa -> pure_lambda ~loc:e.location (optimize_pure_abstraction pa)
+  | PureApply (e1, e2)-> pure_apply ~loc:e.location (optimize_expr e1) (optimize_expr e2)
+  | PureLetIn (e1, pa) -> pure_let_in ~loc:e.location (optimize_expr e1) (optimize_pure_abstraction pa)
   | _ -> e
