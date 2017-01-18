78f92658dbcdb349b599d4de9dee937814ebe053
Matija Pretnar <matija@pretnar.info>
2017-01-18 13:08:13 +0100

    Update pure code generation tests

--- tests/pure-generation/test.sh
+++ tests/pure-generation/test.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 echo "BLANK"
-../eff.native --compile blank.eff
+../../eff.native --compile blank.eff
 echo "INTERPRETER"
-../eff.native --compile interp.eff
+echo "  DIVERGES"
+# ../../eff.native --compile interp.eff
 echo "LOOP"
-../eff.native --compile loop.eff
+../../eff.native --compile loop.eff
 echo "LOOP EFFECTS"
-../eff.native --compile loopEffect.eff
+../../eff.native --compile loopEffect.eff
 echo "PARSER"
 echo "  DIVERGES"
-# ../eff.native --compile parser.eff
+# ../../eff.native --compile parser.eff
 echo "QUEENS"
-../eff.native --compile queens.eff
+../../eff.native --compile queens.eff
