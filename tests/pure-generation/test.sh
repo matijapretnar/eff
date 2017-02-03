0bb3c9b07428a71ceb09a1cff4da21a30f81773e
Matija Pretnar <matija@pretnar.info>
2017-02-03 10:52:43 +0100

    Fix pure generation benchmarks

--- tests/pure-generation/test.sh
+++ tests/pure-generation/test.sh
@@ -3,14 +3,12 @@
 echo "BLANK"
 ../../eff.native --pure --compile blank.eff
 echo "INTERPRETER"
-echo "  DIVERGES"
-# ../../eff.native --pure --compile interp.eff
+../../eff.native --pure --compile interp.eff
 echo "LOOP"
 ../../eff.native --pure --compile loop.eff
 echo "LOOP EFFECTS"
 ../../eff.native --pure --compile loopEffect.eff
 echo "PARSER"
-echo "  DIVERGES"
-# ../../eff.native --pure --compile parser.eff
+../../eff.native --pure --compile parser.eff
 echo "QUEENS"
 ../../eff.native --pure --compile queens.eff
