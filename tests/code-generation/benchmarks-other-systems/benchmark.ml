6127f479c4f24dd287b2c2cecf87d0000efb44a2
Axel Faes <axel.faes@gmail.com>
2017-02-26 16:54:10 +0100

    - update benchmarks to compare the different systems using binaries only

--- tests/code-generation/benchmarks-other-systems/benchmark.ml
+++ tests/code-generation/benchmarks-other-systems/benchmark.ml
@@ -19,6 +19,7 @@ let () =
       Bench.Test.create ~name:"Generated, pure, optimized" (fun () -> QueensOptPure._queens_one_option_94 number_of_queens);
     ]);
   Printf.printf "\n\n"
+
   end;
   if run_queens_one then begin
   Printf.printf "QUEENS ONE CPS BENCHMARK COMPARISON WITH OTHER SYSTEMS (%d queens):\n" number_of_queens;
@@ -43,6 +44,71 @@ let () =
       Bench.Test.create ~name:"Generated, impure, optimized" (fun () -> QueensOptImpure._queens_all_100 number_of_queens);
       Bench.Test.create ~name:"Generated, pure, not optimized" (fun () -> QueensNoOptPure._queens_all_100 number_of_queens); *)
       Bench.Test.create ~name:"Generated, pure, optimized" (fun () -> QueensOptPure._queens_all_100 number_of_queens);
+    ]);
+  Printf.printf "\n\n"
+  end;
+  if run_queens_all then begin
+  Printf.printf "QUEENS ALL BENCHMARK COMPARISON WITH OTHER SYSTEMS (%d queens):\n" 9;
+  Command.run (Bench.make_command [
+      (* Bench.Test.create ~name:"Multicore ocaml" (fun () -> Multicore.queens_all number_of_queens); *)
+      Bench.Test.create ~name:"Eff directly in ocaml" (fun () -> EffInOcaml.queens_all 9);
+      Bench.Test.create ~name:"Handlers in action" (fun () -> HandlersInAction.queens_all 9);
+      (* Bench.Test.create ~name:"Generated, impure, not optimized" (fun () -> QueensNoOptImpure._queens_all_100 number_of_queens);
+      Bench.Test.create ~name:"Generated, impure, optimized" (fun () -> QueensOptImpure._queens_all_100 number_of_queens);
+      Bench.Test.create ~name:"Generated, pure, not optimized" (fun () -> QueensNoOptPure._queens_all_100 number_of_queens); *)
+      Bench.Test.create ~name:"Generated, pure, optimized" (fun () -> QueensOptPure._queens_all_100 9);
+    ]);
+  Printf.printf "\n\n"
+  end;
+  if run_queens_all then begin
+  Printf.printf "QUEENS ALL BENCHMARK COMPARISON WITH OTHER SYSTEMS (%d queens):\n" 10;
+  Command.run (Bench.make_command [
+      (* Bench.Test.create ~name:"Multicore ocaml" (fun () -> Multicore.queens_all number_of_queens); *)
+      Bench.Test.create ~name:"Eff directly in ocaml" (fun () -> EffInOcaml.queens_all 10);
+      Bench.Test.create ~name:"Handlers in action" (fun () -> HandlersInAction.queens_all 10);
+      (* Bench.Test.create ~name:"Generated, impure, not optimized" (fun () -> QueensNoOptImpure._queens_all_100 number_of_queens);
+      Bench.Test.create ~name:"Generated, impure, optimized" (fun () -> QueensOptImpure._queens_all_100 number_of_queens);
+      Bench.Test.create ~name:"Generated, pure, not optimized" (fun () -> QueensNoOptPure._queens_all_100 number_of_queens); *)
+      Bench.Test.create ~name:"Generated, pure, optimized" (fun () -> QueensOptPure._queens_all_100 10);
+    ]);
+  Printf.printf "\n\n"
+  end;
+  if run_queens_all then begin
+  Printf.printf "QUEENS ALL BENCHMARK COMPARISON WITH OTHER SYSTEMS (%d queens):\n" 11;
+  Command.run (Bench.make_command [
+      (* Bench.Test.create ~name:"Multicore ocaml" (fun () -> Multicore.queens_all number_of_queens); *)
+      Bench.Test.create ~name:"Eff directly in ocaml" (fun () -> EffInOcaml.queens_all 11);
+      Bench.Test.create ~name:"Handlers in action" (fun () -> HandlersInAction.queens_all 11);
+      (* Bench.Test.create ~name:"Generated, impure, not optimized" (fun () -> QueensNoOptImpure._queens_all_100 number_of_queens);
+      Bench.Test.create ~name:"Generated, impure, optimized" (fun () -> QueensOptImpure._queens_all_100 number_of_queens);
+      Bench.Test.create ~name:"Generated, pure, not optimized" (fun () -> QueensNoOptPure._queens_all_100 number_of_queens); *)
+      Bench.Test.create ~name:"Generated, pure, optimized" (fun () -> QueensOptPure._queens_all_100 11);
+    ]);
+  Printf.printf "\n\n"
+  end;
+  if run_queens_all then begin
+  Printf.printf "QUEENS ALL BENCHMARK COMPARISON WITH OTHER SYSTEMS (%d queens):\n" 12;
+  Command.run (Bench.make_command [
+      (* Bench.Test.create ~name:"Multicore ocaml" (fun () -> Multicore.queens_all number_of_queens); *)
+      Bench.Test.create ~name:"Eff directly in ocaml" (fun () -> EffInOcaml.queens_all 12);
+      Bench.Test.create ~name:"Handlers in action" (fun () -> HandlersInAction.queens_all 12);
+      (* Bench.Test.create ~name:"Generated, impure, not optimized" (fun () -> QueensNoOptImpure._queens_all_100 number_of_queens);
+      Bench.Test.create ~name:"Generated, impure, optimized" (fun () -> QueensOptImpure._queens_all_100 number_of_queens);
+      Bench.Test.create ~name:"Generated, pure, not optimized" (fun () -> QueensNoOptPure._queens_all_100 number_of_queens); *)
+      Bench.Test.create ~name:"Generated, pure, optimized" (fun () -> QueensOptPure._queens_all_100 12);
+    ]);
+  Printf.printf "\n\n"
+  end;
+  if run_queens_all then begin
+  Printf.printf "QUEENS ALL BENCHMARK COMPARISON WITH OTHER SYSTEMS (%d queens):\n" 13;
+  Command.run (Bench.make_command [
+      (* Bench.Test.create ~name:"Multicore ocaml" (fun () -> Multicore.queens_all number_of_queens); *)
+      Bench.Test.create ~name:"Eff directly in ocaml" (fun () -> EffInOcaml.queens_all 13);
+      Bench.Test.create ~name:"Handlers in action" (fun () -> HandlersInAction.queens_all 13);
+      (* Bench.Test.create ~name:"Generated, impure, not optimized" (fun () -> QueensNoOptImpure._queens_all_100 number_of_queens);
+      Bench.Test.create ~name:"Generated, impure, optimized" (fun () -> QueensOptImpure._queens_all_100 number_of_queens);
+      Bench.Test.create ~name:"Generated, pure, not optimized" (fun () -> QueensNoOptPure._queens_all_100 number_of_queens); *)
+      Bench.Test.create ~name:"Generated, pure, optimized" (fun () -> QueensOptPure._queens_all_100 13);
     ]);
   Printf.printf "\n\n"
   end;
--- tests/code-generation/benchmarks-other-systems/benchmark_multicore.sh
+++ tests/code-generation/benchmarks-other-systems/benchmark_multicore.sh
@@ -5,41 +5,107 @@
 
 cd multicore
 eval `opam config env --switch=4.02.2+multicore` # Multicore ocaml compiler
+echo "compiling multicore native"
 ocamlbuild multicore_cps.native
 ocamlbuild multicore_option.native
 ocamlbuild multicore_all.native
+echo "compiling multicore byte"
+ocamlbuild multicore_cps.byte
+ocamlbuild multicore_option.byte
+ocamlbuild multicore_all.byte
+echo "compiling eff native"
 ocamlbuild queens_cps.native
 ocamlbuild queens_option.native
 ocamlbuild queens_all.native
 cd ..
-
-QUALITY=100
-NUMBER_OF_QUEENS=8
-
-cd multicore
-echo "\n\nMULTICORE"
-echo "Multicore ocaml: ONE QUEENS: CPS ($QUALITY runs)"
-time for ((i=1;i<=$QUALITY;i++)); do ./multicore_cps.native $NUMBER_OF_QUEENS; done
-echo "\n\nMulticore ocaml: ONE QUEENS: OPTION ($QUALITY runs)"
-time for ((i=1;i<=$QUALITY;i++)); do ./multicore_option.native $NUMBER_OF_QUEENS; done
-echo "\n\nMulticore ocaml: ALL QUEENS ($QUALITY runs)"
-time for ((i=1;i<=$QUALITY;i++)); do ./multicore_all.native $NUMBER_OF_QUEENS; done
-
-echo "\n\n\n\nEFF"
-echo "Generated, pure, optimized: ONE QUEENS: CPS ($QUALITY runs)"
-time for ((i=1;i<=$QUALITY;i++)); do ./queens_cps.native $NUMBER_OF_QUEENS; done
-echo "\n\nGenerated, pure, optimized: ONE QUEENS: OPTION ($QUALITY runs)"
-time for ((i=1;i<=$QUALITY;i++)); do ./queens_option.native $NUMBER_OF_QUEENS; done
-echo "\n\nGenerated, pure, optimized: ALL QUEENS ($QUALITY runs)"
-time for ((i=1;i<=$QUALITY;i++)); do ./queens_all.native $NUMBER_OF_QUEENS; done
+eval `opam config env --switch=4.02.2+local-git-`
+cd delimcc
+echo "compiling eff byte"
+ocamlbuild queens_cps.byte
+ocamlbuild queens_option.byte
+ocamlbuild queens_all.byte
+echo "compiling eff directly in ocaml byte"
+ocamlbuild -use-ocamlfind -package delimcc effInOcaml_cps.byte
+ocamlbuild -use-ocamlfind -package delimcc effInOcaml_option.byte
+ocamlbuild -use-ocamlfind -package delimcc effInOcaml_all.byte
+echo "compiling handlers in action byte"
+ocamlbuild -use-ocamlfind -package delimcc handlersInAction_cps.byte
+ocamlbuild -use-ocamlfind -package delimcc handlersInAction_option.byte
+ocamlbuild -use-ocamlfind -package delimcc handlersInAction_all.byte
 cd ..
 
-cd links
-echo "\n\n\n\nLINKS"
-echo "Links: ONE QUEENS: CPS"
-time for ((i=1;i<=$QUALITY;i++)); do ./queens_cps; done
-echo "\n\nLinks: ONE QUEENS: OPTION"
-time for ((i=1;i<=$QUALITY;i++)); do ./queens_option; done
-echo "\n\nLinks: ALL QUEENS"
-time for ((i=1;i<=$QUALITY;i++)); do ./queens_all; done
-cd ..
+QUALITY=5
+#NUMBER_OF_QUEENS=11
+
+for NUMBER_OF_QUEENS in 8 9 10 11 12
+do
+  # cd multicore
+  # echo "\n\nMULTICORE ($NUMBER_OF_QUEENS queens)"
+  # echo "Multicore ocaml: ONE QUEENS: CPS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  # time for ((i=1;i<=$QUALITY;i++)); do ./multicore_cps.native $NUMBER_OF_QUEENS; done
+  # echo "\n\nMulticore ocaml: ONE QUEENS: OPTION ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  # time for ((i=1;i<=$QUALITY;i++)); do ./multicore_option.native $NUMBER_OF_QUEENS; done
+  # echo "\n\nMulticore ocaml: ALL QUEENS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  # time for ((i=1;i<=$QUALITY;i++)); do ./multicore_all.native $NUMBER_OF_QUEENS; done
+  # cd ..
+
+  cd multicore
+  echo "\n\nMULTICORE BYTE ($NUMBER_OF_QUEENS queens)"
+  #echo "Multicore ocaml: ONE QUEENS: CPS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  #time for ((i=1;i<=$QUALITY;i++)); do ./multicore_cps.byte $NUMBER_OF_QUEENS; done
+  #echo "\n\nMulticore ocaml: ONE QUEENS: OPTION ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  #time for ((i=1;i<=$QUALITY;i++)); do ./multicore_option.byte $NUMBER_OF_QUEENS; done
+  echo "\n\nMulticore ocaml: ALL QUEENS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  time for ((i=1;i<=$QUALITY;i++)); do ./multicore_all.byte $NUMBER_OF_QUEENS; done
+  cd ..
+
+  # cd multicore
+  # echo "\n\n\n\nEFF ($NUMBER_OF_QUEENS queens)"
+  # echo "Generated, pure, optimized: ONE QUEENS: CPS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  # time for ((i=1;i<=$QUALITY;i++)); do ./queens_cps.native $NUMBER_OF_QUEENS; done
+  # echo "\n\nGenerated, pure, optimized: ONE QUEENS: OPTION ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  # time for ((i=1;i<=$QUALITY;i++)); do ./queens_option.native $NUMBER_OF_QUEENS; done
+  # echo "\n\nGenerated, pure, optimized: ALL QUEENS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  # time for ((i=1;i<=$QUALITY;i++)); do ./queens_all.native $NUMBER_OF_QUEENS; done
+  # cd ..
+
+  #cd delimcc
+  #echo "\n\n\n\nEFF BYTE ($NUMBER_OF_QUEENS queens)"
+  #echo "Generated, pure, optimized: ONE QUEENS: CPS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  #time for ((i=1;i<=$QUALITY;i++)); do ./queens_cps.byte $NUMBER_OF_QUEENS; done
+  #echo "\n\nGenerated, pure, optimized: ONE QUEENS: OPTION ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  #time for ((i=1;i<=$QUALITY;i++)); do ./queens_option.byte $NUMBER_OF_QUEENS; done
+  #echo "\n\nGenerated, pure, optimized: ALL QUEENS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  #time for ((i=1;i<=$QUALITY;i++)); do ./queens_all.byte $NUMBER_OF_QUEENS; done
+  #cd ..
+
+  #cd delimcc
+  #echo "\n\n\n\nEFF IN OCAML BYTE ($NUMBER_OF_QUEENS queens)"
+  #echo "\n\nEff in OCaml: ONE QUEENS: CPS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  #time for ((i=1;i<=$QUALITY;i++)); do ./effInOcaml_cps.byte $NUMBER_OF_QUEENS; done
+  #echo "\n\nEff in OCaml: ONE QUEENS: OPTION ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  #time for ((i=1;i<=$QUALITY;i++)); do ./effInOcaml_option.byte $NUMBER_OF_QUEENS; done
+  #echo "\n\nEff in OCaml: ALL QUEENS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  #time for ((i=1;i<=$QUALITY;i++)); do ./effInOcaml_all.byte $NUMBER_OF_QUEENS; done
+  #cd ..
+
+  #cd delimcc
+  #echo "\n\n\n\nHANDLERS IN ACTION ($NUMBER_OF_QUEENS queens)"
+  #echo "\n\nHandlers in action: ONE QUEENS: CPS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  #time for ((i=1;i<=$QUALITY;i++)); do ./handlersInAction_cps.byte $NUMBER_OF_QUEENS; done
+  #echo "\n\nHandlers in action: ONE QUEENS: OPTION ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  #time for ((i=1;i<=$QUALITY;i++)); do ./handlersInAction_option.byte $NUMBER_OF_QUEENS; done
+  #echo "\n\nHandlers in action: ALL QUEENS ($NUMBER_OF_QUEENS queens) ($QUALITY runs)"
+  #time for ((i=1;i<=$QUALITY;i++)); do ./handlersInAction_all.byte $NUMBER_OF_QUEENS; done
+  #cd ..
+done
+
+#cd links
+#echo "\n\n\n\nLINKS"
+#echo "Links: ONE QUEENS: CPS"
+#time for ((i=1;i<=$QUALITY;i++)); do ./queens_cps; done
+#echo "\n\nLinks: ONE QUEENS: OPTION"
+#time for ((i=1;i<=$QUALITY;i++)); do ./queens_option; done
+#echo "\n\nLinks: ALL QUEENS"
+#time for ((i=1;i<=$QUALITY;i++)); do ./queens_all; done
+#cd ..
