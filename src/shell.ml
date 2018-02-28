38ce0656447c0fb29f91c4521a6f7d938f3c95e1
Matija Pretnar <matija@pretnar.info>
2018-02-28 11:11:46 +0100

    Enable compilation of external definitions

--- src/shell.ml
+++ src/shell.ml
@@ -199,6 +199,9 @@ let compile_file ppf filename st =
       match OldUtils.lookup f External.values with
       | Some v ->
           let new_ty = ExplicitInfer.source_to_target ty in
+          Print.print out_ppf
+            "let %t = ( %s )" (NewPrint.print_variable x) f ;
+          Format.fprintf out_ppf "\n;;\n " ;
           { st with
             typing= SimpleCtx.extend st.typing x (Type.free_params ty, ty)
           ; explicit_typing=
