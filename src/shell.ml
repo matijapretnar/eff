a58a97ed79bf8dfae2a477cbaac4af1d26b8ae05
Matija Pretnar <matija@pretnar.info>
2018-05-22 15:06:53 +0200

    Rename TypeChecker to TypeSystem
    Because we'll later want to have an EffectSystem as well

--- src/shell.ml
+++ src/shell.ml
@@ -6,17 +6,17 @@ let help_text =
   ^ "#use \"<file>\";;  load commands from file\n"
 
 
-module TypeChecker = SimpleInfer
+module TypeSystem = SimpleInfer
 module Runtime = Eval
 
 type state =
   { desugarer_state: Desugarer.state
-  ; typechecker_state: TypeChecker.state
+  ; type_system_state: TypeSystem.state
   ; runtime_state: Runtime.state }
 
 let initial_state =
   { desugarer_state= Desugarer.initial_state
-  ; typechecker_state= TypeChecker.initial_state
+  ; type_system_state= TypeSystem.initial_state
   ; runtime_state= Runtime.initial_state }
 
 
@@ -28,20 +28,20 @@ let rec exec_cmd ppf st (cmd, loc) =
   match cmd with
   | Commands.Term t ->
       let c = Desugarer.desugar_computation st.desugarer_state t in
-      let typechecker_state, ty =
-        TypeChecker.infer_top_comp st.typechecker_state c
+      let type_system_state, ty =
+        TypeSystem.infer_top_comp st.type_system_state c
       in
       let v = Runtime.run st.runtime_state c in
       Format.fprintf ppf "@[- : %t = %t@]@." (Type.print_beautiful ty)
         (Value.print_value v) ;
-      {st with typechecker_state}
+      {st with type_system_state}
   | Commands.TypeOf t ->
       let c = Desugarer.desugar_computation st.desugarer_state t in
-      let typechecker_state, ty =
-        TypeChecker.infer_top_comp st.typechecker_state c
+      let type_system_state, ty =
+        TypeSystem.infer_top_comp st.type_system_state c
       in
       Format.fprintf ppf "@[- : %t@]@." (Type.print_beautiful ty) ;
-      {st with typechecker_state}
+      {st with type_system_state}
   | Commands.Reset ->
       Format.fprintf ppf "Environment reset." ;
       Tctx.reset () ;
@@ -53,18 +53,18 @@ let rec exec_cmd ppf st (cmd, loc) =
       let eff, (ty1, ty2) =
         Desugarer.desugar_def_effect st.desugarer_state effect_def
       in
-      let typechecker_state =
-        SimpleCtx.add_effect st.typechecker_state eff (ty1, ty2)
+      let type_system_state =
+        SimpleCtx.add_effect st.type_system_state eff (ty1, ty2)
       in
-      {st with typechecker_state}
+      {st with type_system_state}
   | Commands.Quit -> exit 0
   | Commands.Use filename -> execute_file ppf filename st
   | Commands.TopLet defs ->
       let desugarer_state', defs' =
         Desugarer.desugar_top_let st.desugarer_state defs
       in
-      let vars, typechecker_state' =
-        TypeChecker.infer_top_let ~loc st.typechecker_state defs'
+      let vars, type_system_state' =
+        TypeSystem.infer_top_let ~loc st.type_system_state defs'
       in
       let runtime_state' =
         List.fold_right
@@ -84,14 +84,14 @@ let rec exec_cmd ppf st (cmd, loc) =
                 (Value.print_value v) )
         vars ;
       { desugarer_state= desugarer_state'
-      ; typechecker_state= typechecker_state'
+      ; type_system_state= type_system_state'
       ; runtime_state= runtime_state' }
   | Commands.TopLetRec defs ->
       let desugarer_state', defs' =
         Desugarer.desugar_top_let_rec st.desugarer_state defs
       in
-      let vars, typechecker_state' =
-        TypeChecker.infer_top_let_rec ~loc st.typechecker_state defs'
+      let vars, type_system_state' =
+        TypeSystem.infer_top_let_rec ~loc st.type_system_state defs'
       in
       let defs'' = Assoc.of_list defs' in
       let runtime_state' = Runtime.extend_let_rec st.runtime_state defs'' in
@@ -102,7 +102,7 @@ let rec exec_cmd ppf st (cmd, loc) =
             (Type.print_beautiful tysch) )
         vars ;
       { desugarer_state= desugarer_state'
-      ; typechecker_state= typechecker_state'
+      ; type_system_state= type_system_state'
       ; runtime_state= runtime_state' }
   | Commands.External ext_def -> (
       let desugarer_state', (x, ty, f) =
@@ -111,8 +111,8 @@ let rec exec_cmd ppf st (cmd, loc) =
       match Assoc.lookup f External.values with
       | Some v ->
           { desugarer_state= desugarer_state'
-          ; typechecker_state=
-              SimpleCtx.extend st.typechecker_state x (Type.free_params ty, ty)
+          ; type_system_state=
+              SimpleCtx.extend st.type_system_state x (Type.free_params ty, ty)
           ; runtime_state= Runtime.update x v st.runtime_state }
       | None -> Error.runtime "unknown external symbol %s." f )
   | Commands.Tydef tydefs ->
