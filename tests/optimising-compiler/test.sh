5c84cad51b69acb27911a8d355e9ca25515debc3
Brecht Serckx <brecht_serckx@hotmail.com>
2019-04-11 17:55:43 +0200

    test script updates

--- tests/optimising-compiler/test.sh
+++ tests/optimising-compiler/test.sh
@@ -26,10 +26,10 @@ if [ "$1" = "-v" ]; then
   VALIDATE=1
 fi
 
-for FILE in $BASEDIR/*.eff; do
-  "$EFF" --explicit-subtyping -n --compile "$FILE"
+for FILE in "$BASEDIR"/*.eff; do
+  "$EFF" --explicit-subtyping -n --compile "$FILE" &>/dev/null
   mv "$FILE.ml" "$FILE.ml.out"
-  if [ -f $FILE.ml.ref ]; then
+  if [ -f "$FILE.ml.ref" ]; then
     RESULT=$("$DIFF" "$FILE.ml.out" "$FILE.ml.ref")
     if [ "$?" = "0" ]; then
       echo "Passed:  $FILE"
@@ -38,8 +38,8 @@ for FILE in $BASEDIR/*.eff; do
       echo "FAILED:  $FILE"
       if [ $VALIDATE = "1" ]; then
         "$DIFF" "$FILE.ml.out" "$FILE.ml.ref"
-        read -p "Validate $FILE.ml.out as new $FILE.ml.ref? (y/n) [n] " ans
-        if [ "$ans" = "y" -o "$ans" = "Y" ]; then
+        read -pr "Validate $FILE.ml.out as new $FILE.ml.ref? (y/n) [n] " ans
+        if [ "$ans" = "y" ] || [ "$ans" = "Y" ]; then
           mv "$FILE.ml.out" "$FILE.ml.ref"
           echo "Validated: $FILE"
         fi
