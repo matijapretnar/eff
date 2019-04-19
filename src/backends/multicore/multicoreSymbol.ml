8f6c91274ca6c183af82ba1987fc4a9ffc9a87d9
Ziga <ziga.luksic@fmf.uni-lj.si>
2019-04-19 10:41:17 +0200

    Fix shape of code for protected keywords that works with ocamlformat

--- src/backends/multicore/multicoreSymbol.ml
+++ src/backends/multicore/multicoreSymbol.ml
@@ -1,61 +1,11 @@
 let protected =
-  [ "and"
-  ; "as"
-  ; "assert"
-  ; "asr"
-  ; "begin"
-  ; "class"
-  ; "constraint"
-  ; "do"
-  ; "done"
-  ; "downto"
-  ; "else"
-  ; "end"
-  ; "exception"
-  ; "external"
-  ; "false"
-  ; "for"
-  ; "fun"
-  ; "function"
-  ; "functor"
-  ; "if"
-  ; "in"
-  ; "include"
-  ; "inherit"
-  ; "initializer"
-  ; "land"
-  ; "lazy"
-  ; "let"
-  ; "lor"
-  ; "lsl"
-  ; "lsr"
-  ; "lxor"
-  ; "match"
-  ; "method"
-  ; "mod"
-  ; "module"
-  ; "mutable"
-  ; "new"
-  ; "nonrec"
-  ; "object"
-  ; "of"
-  ; "open"
-  ; "or"
-  ; "private"
-  ; "rec"
-  ; "sig"
-  ; "struct"
-  ; "then"
-  ; "to"
-  ; "true"
-  ; "try"
-  ; "type"
-  ; "val"
-  ; "virtual"
-  ; "when"
-  ; "while"
-  ; "with"
-  ; "continue" ]
+  ["and"; "as"; "assert"; "asr"; "begin"; "class"; "constraint"; "do"; "done"]
+  @ ["downto"; "else"; "end"; "exception"; "external"; "false"; "for"; "fun"]
+  @ ["function"; "functor"; "if"; "in"; "include"; "inherit"; "initializer"]
+  @ ["land"; "lazy"; "let"; "lor"; "lsl"; "lsr"; "lxor"; "match"; "method"]
+  @ ["mod"; "module"; "mutable"; "new"; "nonrec"; "object"; "of"; "open"; "or"]
+  @ ["private"; "rec"; "sig"; "struct"; "then"; "to"; "true"; "try"; "type"]
+  @ ["val"; "virtual"; "when"; "while"; "with"; "continue"]
 
 let print_variable var ppf =
   let printer desc n =
