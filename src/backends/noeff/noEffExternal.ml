7f5af059a39e3321e5b31679060865f0fd077417
Žiga Zupančič <zigazupancic5@gmail.com>
2019-11-04 18:02:06 +0100

    .

--- src/backends/noeff/noEffExternal.ml
+++ src/backends/noeff/noEffExternal.ml
@@ -6,12 +6,6 @@ type translation = Exists of string | Unknown
 let comparison_functions =
   Assoc.of_list [("=", Exists "(=)"); ("<", Exists "(<)")]
 
-let constants =
-  Assoc.of_list
-    [ ("infinity", Exists "infinity")
-    ; ("neg_infinity", Exists "neg_infinity")
-    ; ("nan", Exists "nan") ]
-
 let arithmetic_operations =
   Assoc.of_list
     [ ("~-", Exists "(~-)")
@@ -38,17 +32,6 @@ let arithmetic_operations =
     ; ("atan", Exists "atan")
     ; ("sqrt", Exists "sqrt") ]
 
-let string_operations =
-  Assoc.of_list [("^", Exists "(^)"); ("string_length", Exists "String.length")]
-
-let conversion_functions =
-  Assoc.of_list
-    [ ("to_string", Unknown)
-    ; ("string_of_float", Exists "string_of_float")
-    ; ("string_of_int", Exists "string_of_int")
-    ; ("float_of_int", Exists "float_of_int")
-    ; ("int_of_float", Exists "int_of_float") ]
-
 let top_handler =
   "(fun c ->" ^ "  match c () with\n"
   ^ "  | effect (Print s) k -> (print_string s; continue k ())\n"
@@ -61,8 +44,6 @@ let other =
     [("failwith", Exists "failwith"); ("_ocaml_tophandler", Exists top_handler)]
 
 let values =
-  comparison_functions |> Assoc.concat constants
+  comparison_functions
   |> Assoc.concat arithmetic_operations
-  |> Assoc.concat string_operations
-  |> Assoc.concat conversion_functions
   |> Assoc.concat other
--- src/backends/noeff/noEffPrint.ml
+++ src/backends/noeff/noEffPrint.ml
@@ -52,7 +52,35 @@ let pp_tyname ty_name ppf = CoreTypes.TyName.print ty_name ppf
 
 let pp_typaram ty_param ppf = print ppf "'ty%t" (CoreTypes.TyParam.print ~safe:true ty_param)
 
-let pp_variable var ppf = CoreTypes.Variable.print var ppf
+let protected =
+  ["and"; "as"; "assert"; "asr"; "begin"; "class"; "constraint"; "do"; "done"]
+  @ ["downto"; "else"; "end"; "exception"; "external"; "false"; "for"; "fun"]
+  @ ["function"; "functor"; "if"; "in"; "include"; "inherit"; "initializer"]
+  @ ["land"; "lazy"; "let"; "lor"; "lsl"; "lsr"; "lxor"; "match"; "method"]
+  @ ["mod"; "module"; "mutable"; "new"; "nonrec"; "object"; "of"; "open"; "or"]
+  @ ["private"; "rec"; "sig"; "struct"; "then"; "to"; "true"; "try"; "type"]
+  @ ["val"; "virtual"; "when"; "while"; "with"; "continue"]
+
+let pp_variable var ppf = 
+let printer desc n =
+    (* [mod] has privileges because otherwise it's stupid *)
+    if desc = "mod" then Format.fprintf ppf "_op_%d (* %s *)" n desc
+    else (
+      if List.mem desc protected then
+        Print.warning
+          "Warning: Protected keyword [%s]. Must be fixed by hand!@." desc ;
+      match desc.[0] with
+      | 'a' .. 'z' | '_' -> Format.fprintf ppf "%s" desc
+      | '$' -> (
+        match desc with
+        | "$c_thunk" -> Format.fprintf ppf "_comp_%d" n
+        | "$id_par" -> Format.fprintf ppf "_id_%d" n
+        | "$anon" -> Format.fprintf ppf "_anon_%d" n
+        | "$bind" -> Format.fprintf ppf "_b_%d" n
+        | _ -> Format.fprintf ppf "_x_%d" n )
+      | _ -> Format.fprintf ppf "_op_%d (* %s *)" n desc )
+  in
+  CoreTypes.Variable.fold printer var
 
 let pp_field pp sep (field, value) ppf =
   print ppf "%t %s %t" (CoreTypes.Field.print field) sep (pp value)
