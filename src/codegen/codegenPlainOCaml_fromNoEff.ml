a6fa07461a745da23e21186e982dfd50c27cf294
Stien <stien.vanderhallen@hotmail.com>
2020-03-25 16:24:17 +0100

    NoEff to plain OCaml

--- src/codegen/codegenPlainOCaml_fromNoEff.ml
+++ src/codegen/codegenPlainOCaml_fromNoEff.ml
@@ -1,108 +1,63 @@
 module NoEff = NoEffSyntax
 module OCamn = OcamlSyntax
 
-let print_variable = NoEff.Variable.print ~safe:true
+let rec elab_pattern in_p =
+  match in_p with
+  | NoEff.PNVar x -> OCaml.PVar x
+  | NoEff.PNAs (p, x) -> OCaml.PAs (elab_pattern p, x)
+  | NoEff.PNConst c -> OCaml.PConst c
+  | NoEff.PNTuple lst -> OCaml.PTuple (List.map elab_pattern lst)
+  | NoEff.PNRecord lst -> OCaml.PRecord
+      (Assoc.map (fun (f, p) -> (f, elab_pattern p)) lst)
+  | NoEff.PNVariant (lbl, None) -> OCaml.PVariant (lbl, None)
+  | NoEff.PNVariant (lbl, Some p) -> OCaml.PVariant (lbl, Some (elab_pattern p))
+  | NoEff.PNNonbinding -> OCaml.PNonbinding
 
-let print_effect (eff, _) ppf = Print.print ppf "Effect_%s" eff
-
-let rec print_pattern ?max_level p ppf =
-  let print ?at_level = Print.print ?max_level ?at_level ppf in
-  match p with
-  | NoEff.PNVar x -> print "%t" (print_variable x)
-  | NoEff.PNAs (p, x) ->
-      print "%t as %t" (print_pattern p) (print_variable x)
-  | NoEff.PNConst c -> Const.print c ppf
-  | NoEff.PNTuple lst -> Print.tuple print_pattern lst ppf
-  | NoEff.PNRecord lst -> Print.record print_pattern lst ppf
-  | NoEff.PNVariant (lbl, None) when lbl = CoreTypes.nil -> print "[]"
-  | NoEff.PNVariant (lbl, None) -> print "%s" lbl
-  | NoEff.PNVariant ("(::)", Some NoEff.PNTuple [p1; p2]) ->
-      print ~at_level:1 "((%t) :: (%t))" (print_pattern p1) (print_pattern p2)
-  | NoEff.PNVariant (lbl, Some p) ->
-      print ~at_level:1 "(%s @[<hov>%t@])" lbl (print_pattern p)
-  | NoEff.PNNonbinding -> print "_"
-
-
-let rec print_term ?max_level e ppf =
-  let print ?at_level = Print.print ?max_level ?at_level ppf in
-  match e with
-  | NoEff.NVar x -> print "%t" (print_variable x)
-  | NoEff.NConst c -> print "%t" (Const.print c)
-  | NoEff.NTuple lst -> Print.tuple print_term lst ppf
-  | NoEff.NRecord lst -> Print.record print_term lst ppf
-  | NoEff.NVariant (lbl, None) -> print "%s" lbl
-  | NoEff.NVariant (lbl, Some e) ->
-      print ~at_level:1 "(%s %t)" lbl (print_term e)
-  (* STIEN: Need to keep track of type? *)
+let rec elab_term t_in =
+  match t_in with
+  | NoEff.NVar x -> OCaml.Var x
+  | NoEff.NConst c -> OCaml.Const c
+  | NoEff.NTuple lst -> OCaml.Tuple (List.map elab_term lst)
+  | NoEff.NRecord lst -> OCaml.Record
+      (Assoc.map (fun (f, t) -> (f, elab_term t)) lst)
+  | NoEff.NVariant (lbl, None) -> OCaml.Variant (lbl, None)
+  | NoEff.NVariant (lbl, Some t) -> OCaml.Variant (lbl, Some (elab_term t))
   | NoEff.NFun (x, _, c) ->
-      print "fun (%t) -> %t" (print_pattern x) (print_term c)
-  | NoEff.NEffect eff -> print ~at_level:2 "effect %t" (print_effect eff)
-  (* STIEN: TODO *)
-  | Erasure.EHandler h ->
+      OCaml.Lambda (elab_pattern x, elab_term c)
+  | NoEff.NEffect eff -> OCaml.Effect eff
+  (* TODO *)
+  | NoEff.NHandler h ->
       print ~at_level:2
         "fun c -> handler {@[<hov> value_clause = (@[fun %t@]);@ effect_clauses = (fun (type a) (type b) (x : (a, b) effect) ->\n             ((match x with %t) : a -> (b -> _ computation) -> _ computation)) @]} c"
         (print_abstraction_with_ty h.Erasure.value_clause)
         (print_effect_clauses (Assoc.to_list h.Erasure.effect_clauses))
   | NoEff.NLetVal (p, e, c) ->
-      print ~at_level:2 "let @[<hov>%t =@ %t@ in@]@ %t" (print_pattern p)
-        (print_term e) (print_term c)
+      OCaml.Let ((elab_pattern p, elab_term e), elab_term c)
   | NoEff.NApplyTerm (e1, e2) ->
-      print ~at_level:1 "%t@ %t"
-        (print_term ~max_level:1 e1)
-        (print_term ~max_level:0 e2)
-  (* STIEN: TODO *)
-  | Erasure.EHandle (e, c) ->
-      print ~at_level:1 "%t %t"
-        (print_expression ~max_level:0 e)
-        (print_computation ~max_level:0 c)
-  | NoEff.NCall (eff, e, a) ->
-      print ~at_level:1 "call %t %t (@[fun %t@])" (print_effect eff)
-        (print_term ~max_level:0 e)
-        (print_abstraction_with_ty a)
+      OCaml.Apply (elab_term e1, elab_term e2)
+  | NoEff.Handle (e, c) ->
+      OCaml.Apply (elab_term e, elab_term c)
+  | NoEff.NCall (eff, e, (p, _, c)) ->
+      OCaml.Call (eff, elab_term e, (elab_pattern p, elab_pattern c))
+  (* TODO *)
   | NoEff.NBind (c1, a) ->
       print ~at_level:2 "@[<hov>%t@ >>@ @[fun %t@]@]"
         (print_term ~max_level:0 c1)
         (print_abstraction a)
   | NoEff.NMatch (e, alist) ->
-      print ~at_level:1 "match %t with %t"
-        (print_term ~max_level:0 e)
-        (print_cases alist)
-  (* STIEN: Keep track of type? *)
-  | NoEff.NLetRec ([(var, _, e)], c) ->
-      print ~at_level:2 "let rec @[<hov>%t =@ %t@ in@]@ %t"
-        (print_variable var) (print_term e) (print_term c)
-  (* STIEN: NoEff.NOp: faalt in erasure exeff -> skeleff, wat hier doen? *)
-  (* STIEN: NoEff.NBigLambdaCoer - cf. coercions *)
-  (* STIEN: NoEff.NApplyCoer - cf. coercions*)
-  (* STIEN: NoEff.NCast - cf. coercions *)
-  (* STIEN: NoEff.NReturn - zou makkelijk moeten zijn, wel syntax checken *)
+      OCaml.Match (elab_term e,
+        List.map (fun (p, c) ->
+          OCaml.ValueClause (elab_pattern p, elab_pattern c)) alist)
+  | NoEff.NLetRec (alist, c) ->
+      let alist' = List.map (fun (v, _, _, (p, c))
+                  -> (v, (elab_pattern p, elab_pattern c))) alist in
+      OCaml.LetRec (alist*, elab_term c)
+  | NoEff.NOp (eff, t) -> failwith "No operations supposed to happen"
+  | NoEff.NBigLambdaCoer (p, c, t)-> failwith "Not supported without polymorphism"
+  | NoEff.NApplyCoer (t, c) -> failwith "Not supported without polymorphism"
+  | NoEff.NCast (t, c) -> OCaml.Apply (elab_coercion c, elab_term t)
+  | NoEff.NReturn t -> OCaml.Return (elab_term t)
 
-and print_effect_clauses eff_clauses ppf =
-  let print ?at_level = Print.print ?at_level ppf in
-  match eff_clauses with
-  | [] -> print "| eff' -> fun arg k -> Call (eff', arg, k)"
-  | (((eff_name, (t1, t2)) as eff), (p1, p2, c)) :: cases ->
-      print ~at_level:1
-        "| %t -> (fun (%t : %t) (%t : %t -> _ computation) -> %t) %t"
-        (print_effect eff) (print_pattern p1) (Types.print_target_ty t1)
-        (print_pattern p2) (Types.print_target_ty t2) (print_term c)
-        (print_effect_clauses cases)
-
-and print_cases cases ppf =
-  let print ?at_level = Print.print ?at_level ppf in
-  match cases with
-  | [] -> ()
-  | (p, c) :: cases ->
-      print ~at_level:1 "| %t -> %t %t" (print_pattern p) (print_term c)
-        (print_cases cases)
-
-and print_abstraction (p, c) ppf =
-  Format.fprintf ppf "%t ->@;<1 2> %t" (print_pattern p) (print_term c)
-
-and print_abstraction_with_ty (p, _, c) ppf =
-  Format.fprintf ppf "%t ->@;<1 2> %t" (print_pattern p) (print_term c)
-
-(* ELABORATING COERCIONS AS FUNCTIONS *)
 and elab_coercion (coer: NoEff.n_coercion): OCaml.term =
   match coer with
   | NoEff.NCoerRefl t ->
@@ -119,8 +74,8 @@ and elab_coercion (coer: NoEff.n_coercion): OCaml.term =
         let x2 = CoreTypes.Variable.new_fresh in
         OCaml.Lambda (OCaml.PVar x1,
           OCaml.Lambda (OCaml.PVar x2,
-            OCaml.Apply (g, OCaml.Apply x1, OCaml.Apply (f, x2))))
-    | _ -> /* STIEN: fail here */
+            OCaml.Apply (g, OCaml.Apply (OCaml.Var x1, OCaml.Apply (f, OCaml.Var x2)))))
+    | _ -> failwith "Wrong arrow coercion elaboration"
   | NoEff.NCoerHandler (arg, res) ->
     let f = elab_coercion arg in
     let g = elab_coercion res in
@@ -132,21 +87,57 @@ and elab_coercion (coer: NoEff.n_coercion): OCaml.term =
         let x2 = CoreTypes.Variable.new_fresh in
         OCaml.Lambda (OCaml.PVar x1,
           OCaml.Lambda (OCaml.PVar x2,
-            // TODO
-    | _ -> /* STIEN: fail here */
+            OCaml.Apply (g,
+              OCaml.Apply (OCaml.PVar x1, OCaml.Return (OCaml.Apply (f, OCaml.Var x2))))
+    | _ -> failwith "Wrong handler coercion elaboration"
   | NoEff.NCoerHandToFun (arg, res) ->
-    let arg_fun
+    let f = elab_coercion arg in
+    let g = elab_coercion res in
+    let x1 = CoreTypes.Variable.new_fresh in
+    let x2 = CoreTypes.Variable.new_fresh in
+    OCaml.Lambda (OCaml.PVar x1,
+      OCaml.Lambda (OCaml.PVar x2,
+        OCaml.Apply (g,
+          OCaml.Apply (OCaml.Var x1, OCaml.Return (OCaml.Apply (f, OCaml.Var x2))))))
   | NoEff.NCoerFunToHand (arg, res) ->
-  | NoEff.NCoerQual
+    let f = elab_coercion arg in
+    let g = elab_coercion res in
+    let x1 = CoreTypes.Variable.new_fresh in
+    let x2 = CoreTypes.Variable.new_fresh in
+    OCaml.Lambda (OCaml.PVar x1, OCaml.Lambda (OCaml.PVar x2,
+      OCaml.Match (OCaml.Var x2,
+        (OCaml.ValueClause (OCaml.PReturn y,
+          OCaml.Apply (g, OCaml.Apply (OCaml.Var x1, OCaml.Apply (f, elab_term y)))))
+    )))
+  | NoEff.NCoerQual -> failwith "Dropped when dropping polymorphism"
   | NoEff.NCoerReturn c ->
-    let (pat, comp) = elab_coercion c in
-    (pat, NoEff.NReturn comp)
+    let f = elab_coercion c in
+    let x = CoreTypes.Variable.new_fresh in
+    match f with
+    | OCaml.Lambda (p_f, c_f) ->
+        OCaml.Lambda (OCaml.PVar x, OCaml.Return
+          (OCaml.Apply (f, OCaml.Var x)))
+    | _ -> failwith "Wrong return coercion elaboration"
   | NoEff.NCoerComp c ->
-    fun m -> m >>= \x -> NoEff.NReturn (NoEff.NApplyTerm )
+    let f = elab_coercion c in
+    let x = CoreTypes.Variable.new_fresh in
+      OCaml.Lambda (PVar x,
+        OCaml.Match (OCaml.Var x,
+          (OCaml.ValueClause (OCaml.PReturn y, OCaml.Return
+            (OCaml.Apply (f, y))))
+      fun m -> m >>= \x -> NoEff.NReturn (NoEff.NApplyTerm )
   | NoEff.NCoerUnsafe c ->
-    fun x ->
-      match x with
-      | NoEff.NReturn y -> g y
-      | _ -> error
-  | NoEff.NCoerApp (c1, c2)
-  | NoEff.NCoerTrans (c1, c2)
+    let f = elab_coercion c in
+    match f with
+    | OCaml.Lambda (f_pat, f_comp) ->
+      let x = CoreTypes.Variable.new_fresh in
+      OCaml.Lambda (OCaml.PVar x,
+        OCaml.Match (OCaml.Var x,
+          (OCaml.ValueClause (OCaml.PReturn y, OCaml.Apply (f, elab_term y)))
+  | NoEff.NCoerApp (c1, c2) -> failwith "Dropped when dropping polymorphism"
+  | NoEff.NCoerTrans (c1, c2) ->
+    let f = elab_coercion c1 in
+    let g = elab_coercion c2 in
+    let x = CoreTypes.Variable.new_fresh in
+    OCaml.Lambda (OCaml.PVar x,
+      OCaml.Apply (f, OCaml.Apply (g, OCaml.Var x)))
