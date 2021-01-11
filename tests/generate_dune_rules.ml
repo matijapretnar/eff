fb3c4fbf3e38904a0fa8bebb071fa7e5c1930b09
Filip Koprivec <koprivec.filip@gmail.com>
2021-01-12 00:53:37 +0100

    Added old tets for explicit inference

--- tests/generate_dune_rules.ml
+++ tests/generate_dune_rules.ml
@@ -5,6 +5,14 @@ let src_suffix = ".eff"
 
 let test_script = "regression_test"
 
+type config = { allowed_exit_code : int; args : string }
+
+let parse_config =
+  let l = Array.length Sys.argv in
+  let allowed_exit_code = if l >= 2 then int_of_string Sys.argv.(1) else 0 in
+  let args = if l >= 3 then Sys.argv.(2) ^ " " else "" in
+  { allowed_exit_code; args }
+
 let skip = []
 
 let generate_empty_ref = true
@@ -12,7 +20,7 @@ let generate_empty_ref = true
 (* Placeholder for future use *)
 let global_stanza _files = ()
 
-let test_case_rule_stanza exit_code (_bare, full_name) =
+let test_case_rule_stanza config (_bare, full_name) =
   Printf.printf "(rule\n";
   Printf.printf " (deps\n";
   Printf.printf " %%{bin:eff}\n";
@@ -21,21 +29,14 @@ let test_case_rule_stanza exit_code (_bare, full_name) =
   Printf.printf "    (action\n";
   Printf.printf "     (with-outputs-to \"%%{target}\"\n";
   Printf.printf "      (with-accepted-exit-codes\n";
-  Printf.printf "       %d\n" exit_code;
-  Printf.printf "       (run eff \"./%s\")))))\n\n" full_name
+  Printf.printf "       %d\n" config.allowed_exit_code;
+  Printf.printf "       (run eff %s\"./%s\")))))\n\n" config.args full_name
 
 let test_case_alias_stanza (_bare, full_name) =
   Printf.printf "(rule\n";
   Printf.printf " (alias runtest)\n";
   Printf.printf "  (action\n";
   Printf.printf "   (diff \"%s.ref\" \"%s.out\")))\n\n" full_name full_name
-
-type config = { allowed_exit_code : int }
-
-let parse_config =
-  let l = Array.length Sys.argv in
-  let allowed_exit_code = if l >= 2 then int_of_string Sys.argv.(1) else 0 in
-  { allowed_exit_code }
 
 let main () =
   let config = parse_config in
@@ -51,7 +52,7 @@ let main () =
       global_stanza tests;
       List.iter
         (fun test ->
-          test_case_rule_stanza config.allowed_exit_code test;
+          test_case_rule_stanza config test;
           test_case_alias_stanza test)
         tests
 
