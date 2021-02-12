8abf4ce99dc838fb9f486f54ed5129b997e158c3
Matija Pretnar <matija@pretnar.info>
2021-02-12 14:05:27 +0100

    Add schemes to typing environment

--- src/04-backends/explicit-effects/typingEnv.ml
+++ src/04-backends/explicit-effects/typingEnv.ml
@@ -1,16 +1,9 @@
 open Utils
 
-type t = (Language.CoreTypes.Variable.t, Type.ty) Assoc.t
+type t = (Language.CoreTypes.Variable.t, Type.ty_scheme) Assoc.t
 
 let empty = Assoc.empty
 
 let lookup ctx x = Assoc.lookup x ctx
 
 let update ctx x sch = Assoc.update x sch ctx
-
-let return_context ctx = Assoc.to_list ctx
-
-let apply_sub ctx sub =
-  Assoc.kmap
-    (fun (var, ty) -> (var, Substitution.apply_substitutions_to_type sub ty))
-    ctx
