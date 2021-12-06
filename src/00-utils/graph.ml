fabf0fb801a0386701811ef5892fb08266867d30
Filip Koprivec <koprivec.filip@gmail.com>
2021-12-06 17:41:48 +0100

    Detect scc

--- src/00-utils/graph.ml
+++ src/00-utils/graph.ml
@@ -34,6 +34,80 @@ struct
 
   let fold f graph acc = Vertex.Map.fold (fun v -> Edges.fold (f v)) graph acc
 
+  let vertices graph =
+    let vertices =
+      fold
+        (fun source sink _ vertices ->
+          vertices |> Vertex.Set.add source |> Vertex.Set.add sink)
+        graph Vertex.Set.empty
+    in
+    vertices
+
+  let reverse graph =
+    fold
+      (fun source sink edge -> add_edge sink source edge)
+      graph Vertex.Map.empty
+
+  let traverse source graph =
+    let rec traverse' visited source =
+      let edges = get_edges source graph in
+      Edges.fold
+        (fun target _ visited ->
+          if Vertex.Set.mem target visited then visited
+          else traverse' (visited |> Vertex.Set.add target) target)
+        edges visited
+    in
+    traverse' (Vertex.Set.singleton source) source
+
+  let component graph reverse source =
+    Vertex.Set.inter (traverse source graph) (traverse source reverse)
+
+  let scc graph =
+    let reversed = reverse graph in
+    let visited = Vertex.Set.empty in
+    let components = [] in
+    let _, components =
+      Vertex.Set.fold
+        (fun source (visited, components) ->
+          if Vertex.Set.mem source visited then (visited, components)
+          else
+            let c = component graph reversed source in
+            (visited |> Vertex.Set.union c, c :: components))
+        (vertices graph) (visited, components)
+    in
+    (* TODO: a bunch of asserts *)
+    components
+
+  let toposort graph =
+    let permanent = Vertex.Set.empty in
+    let temp = Vertex.Set.empty in
+    let l = [] in
+    let rec visit n (temp, permanent, l) =
+      if Vertex.Set.mem n permanent then (temp, permanent, l)
+      else (
+        (* Check for cycle -> Should not happen *)
+        assert (Vertex.Set.mem n temp |> not);
+
+        let temp = Vertex.Set.add n temp in
+        let outgoing = graph |> Vertex.Map.find n in
+        let temp, permanent, l =
+          Vertex.Set.fold
+            (fun m (temp, permanent, l) -> visit m (temp, permanent, l))
+            outgoing (temp, permanent, l)
+        in
+        let temp = Vertex.Set.remove n temp in
+        let permanent = Vertex.Set.add n permanent in
+        (temp, permanent, n :: l))
+    in
+    let _, _, l =
+      Vertex.Map.fold
+        (fun f _ (temp, permanent, l) ->
+          if Vertex.Set.mem f permanent then (temp, permanent, l)
+          else visit f (temp, permanent, l))
+        graph (temp, permanent, l)
+    in
+    l
+
   let print_node node ppf =
     let vertex = Vertex.print node in
     Print.print ppf "node_%t[label=\"%t\"];" vertex vertex
@@ -42,17 +116,29 @@ struct
     Print.print ppf "@[<h>node_%t -> node_%t [label=\"%t\"]@]" (Vertex.print v1)
       (Vertex.print v2) (Edge.print edge)
 
+  let print_node_component cluster_name (ind, cmp) ppf =
+    if Vertex.Set.cardinal cmp = 1 then
+      Print.print ppf "%t" (print_node (Vertex.Set.choose cmp))
+    else
+      Print.print ppf
+        "subgraph cluster_%t_%d {label=\"\"; graph[style=dotted]; \n %t \n}"
+        cluster_name ind
+        (Print.sequence "\n" print_node (Vertex.Set.elements cmp))
+
   let print_dot graph cluster_name header ppf =
-    let vertices, edges =
+    let _vertices, edges =
       fold
         (fun source sink edge (vertices, edges) ->
           ( vertices |> Vertex.Set.add source |> Vertex.Set.add sink,
             (source, edge, sink) :: edges ))
         graph (Vertex.Set.empty, [])
     in
-    let vertices = Vertex.Set.elements vertices in
+    let components = scc graph in
+    (* let vertices = Vertex.Set.elements vertices in *)
     Print.print ppf "subgraph %t{\n%t\n//nodes\n%t\n\n//edges\n%t\n\n}\n"
       cluster_name header
-      (Print.sequence "\n" print_node vertices)
+      (Print.sequence "\n"
+         (print_node_component cluster_name)
+         (List.mapi (fun x i -> (x, i)) components))
       (Print.sequence "\n" print_edge edges)
 end
