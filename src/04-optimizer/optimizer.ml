4c47fe073162a076bb6a39bff2cc6c4fdc611f26
Matija Pretnar <matija@pretnar.info>
2024-06-14 09:01:04 +0200

    Remove unnecessary special case

--- src/04-optimizer/optimizer.ml
+++ src/04-optimizer/optimizer.ml
@@ -331,10 +331,6 @@ and handle_computation state hnd comp =
           (handle_abstraction state hnd abs)
       in
       handle_computation state hnd' cmp
-  | CastComp (cmp, { term = tcoer, dcoer; _ })
-    when Coercion.is_trivial_ty_coercion tcoer ->
-      let hnd' = Term.handler_with_smaller_input_dirt hnd dcoer in
-      handle_computation state hnd' cmp
   | CastComp (cmp, { term = tcoer, dcoer; _ }) ->
       let ty, _ = cmp.ty in
       let x_pat, x_var = Term.fresh_variable "x" ty in
